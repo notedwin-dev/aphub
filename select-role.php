4fe0e97ebceb7585fb7efd5a843ae40b5c59f821
Alvislai04 <alvislai25@gmail.com>
2024-01-02 19:26:20 +0800

    select role basecode

---
+++ select-role.php
@@ -0,0 +1,29 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Role Selection</title>
+    <style>
+            #favicon {
+            padding-bottom: 5px;
+            border-radius: 10px;
+            border: none;
+            width: fit-content;
+            height: 32px;
+            justify-content: center;
+        }
+    </style>
+ </head>
+ <body>
+    <img src="home/logo.png" id="favicon">
+    <h1>Select your account type!</h1>
+    <button>
+        I'm a Lecturer!
+    </button>
+    <h1>OR</h1>
+    <button>
+        I'm a Student!
+    </button>
+ </body>
+ </html>
